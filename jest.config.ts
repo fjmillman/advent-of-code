4b6f60c1baba56390f62d2daa79adf99c8adf000
Freddie John Millman <freddie.millman@icloud.com>
2021-12-15 22:28:03 +0000

    Setup project to run TS scripts with jest testing

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { InitialOptionsTsJest } from 'ts-jest';
+
+const config: InitialOptionsTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testEnvironment: 'node',
+};
+
+export default config;
